4c03147019fc382a18d5b99605c03b74d39d314b
meckblvck <meckydrix@gmail.com>
2017-12-08 20:56:33 +0100

    Added RMS_Bank model

---
+++ RMS.Data.Objects/Entities.RMS/RMS_Bank.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Data.Objects.Entities.RMS
+{
+    class RMS_Bank
+    {
+        [Key]
+        public long RMS_Bank { get; set; }
+
+        [Display(Name="Name")]
+        [Required(ErrorMessage="Bank name is required")]
+        public string Name { get; set; }
+    }
+}
